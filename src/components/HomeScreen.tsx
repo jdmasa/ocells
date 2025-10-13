23ccee7c103477d71903cf20421a1a9e42bba4d7
jdmasa <47989615+jdmasa@users.noreply.github.com>
2025-10-13 19:52:48 +0200

    Updated HomeScreen.tsx

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -24,7 +24,7 @@ export function HomeScreen({ profile, onStartNewSession, onLoadSession, onLogout
               Què vols fer avui?
             </p>
           </div>
- Fotos de https://ornitologia.org/ca/fitxa_jardins.pdf
+
           <div className="space-y-4">
             <button
               onClick={onStartNewSession}
